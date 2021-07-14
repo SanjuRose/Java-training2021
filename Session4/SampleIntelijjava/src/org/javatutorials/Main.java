f993f5fbfcba452dde7e2c7ce534b281278598ee
SanjuMathew <sanjumathew@155gmail.com>
2021-07-14 18:21:31 +0530

    IntelijJava_Sample

---
+++ Session4/SampleIntelijjava/src/org/javatutorials/Main.java
@@ -0,0 +1,10 @@
+package org.javatutorials;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name="Sanju";
+        System.out.println("Hello--" + name);
+    }
+}
